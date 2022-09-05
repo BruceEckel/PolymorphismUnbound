e938d8b947ab3709593b4cd75166425fd4eb49eb
Bruce Eckel <BruceTEckel@gmail.com>
2022-09-05 11:04:24 -0600

    Create SumtypePolymorphism.scala

---
+++ src/scala/SumtypePolymorphism.scala
@@ -0,0 +1,35 @@
+package sumtype
+
+case class Dog():
+    def eat() = println("eating dog food")
+    def bark() = println("woof")
+
+case class Person():
+    def scarf() = println("eating pizza")
+    def talk() = println("hello")
+
+case class Robot():
+    def charge() = println("recharging")
+    def communicate() = println("operational")
+
+case class Slug():
+    def absorb() = println("eating grass")
+
+def demo(o: Dog | Person | Robot | Slug): Unit =
+    o match
+        case d: Dog =>
+            d.eat()
+            d.bark()
+        case p: Person =>
+            p.scarf()
+            p.talk()
+        case r: Robot =>
+            r.charge()
+            r.communicate()
+        case s: Slug =>
+            s.absorb()
+
+@main def main() =
+    val list: List[Dog | Person | Robot | Slug] =
+        List(Dog(), Person(), Robot(), Slug())
+    list.foreach(o => demo(o))
