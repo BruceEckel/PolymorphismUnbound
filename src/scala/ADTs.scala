a7f75aaaa039b791940f3aa1886daae23dada62b
Bruce Eckel <BruceTEckel@gmail.com>
2022-09-05 15:34:13 -0600

    Create ADTs.scala

---
+++ src/scala/ADTs.scala
@@ -0,0 +1,24 @@
+// src/scala/ADTs.scala
+// Algebraic Data Types
+package adts
+import ADT._
+
+enum ADT:
+    case Dog, Person, Robot, Slug
+    def consume() = this match
+        case Dog => println("eating dog food")
+        case Person => println("eating pizza")
+        case Robot => println("charging")
+        case Slug => println("eating grass")
+    def communicate() = this match
+        case Dog => println("woof")
+        case Person => println("hello")
+        case Robot => println("operational")
+        case Slug => ()
+
+def demo(x: ADT): Unit =
+    x.consume()
+    x.communicate()
+
+@main def main() =
+    List(Dog, Person, Robot, Slug).foreach(demo(_))
