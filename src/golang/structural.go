88cd75bcce6cf35638547c1aec0d6fd2b3edd237
Jack Leow <jleow@goodrx.com>
2022-09-07 17:01:21 -0700

    Added Go structural typing example.

---
+++ src/golang/structural.go
@@ -0,0 +1,40 @@
+package main
+
+type Dog struct{}
+
+func (_ Dog) Consume()     { println("eating dog food") }
+func (_ Dog) Communicate() { println("woof") }
+
+type Person struct{}
+
+func (_ Person) Consume()     { println("eating pizza") }
+func (_ Person) Communicate() { println("hello") }
+
+type Robot struct{}
+
+func (_ Robot) Consume()     { println("charging") }
+func (_ Robot) Communicate() { println("operational") }
+
+type Slug struct{}
+
+func (_ Slug) Consume()     { println("eating grass") }
+func (_ Slug) Communicate() {}
+
+type ConsumerCommunicator interface {
+	Consume()
+	Communicate()
+}
+
+func Poly(x ConsumerCommunicator) {
+	x.Consume()
+	x.Communicate()
+}
+
+func main() {
+	subjects := []ConsumerCommunicator{
+		Dog{}, Person{}, Robot{}, Slug{},
+	}
+	for _, subject := range subjects {
+		Poly(subject)
+	}
+}
