51df4359ef36fe56ce9621211193cb369ee3ac0e
Bruce Eckel <BruceTEckel@gmail.com>
2022-09-08 22:05:19 -0600

    Update generic.go
    Trimmed down the example to its essence. I think it still gets the idea across.

--- src/golang/generic/generic.go
+++ src/golang/generic/generic.go
@@ -2,29 +2,32 @@ package main
 
 type Dog struct{}
 
-func (_ Dog) Eat()     { println("eating dog food") }
+func (_ Dog) Eat()  { println("eating dog food") }
 func (_ Dog) Bark() { println("woof") }
 
 type Person struct{}
 
-func (_ Person) Eat()     { println("eating pizza") }
+func (_ Person) Eat()   { println("eating pizza") }
 func (_ Person) Greet() { println("hello") }
 
 type Robot struct{}
 
-func (_ Robot) Eat()     { println("charging") }
+func (_ Robot) Eat()      { println("charging") }
 func (_ Robot) Initiate() { println("operational") }
 
 type Slug struct{}
 
-func (_ Slug) Eat()     { println("eating grass") }
+func (_ Slug) Eat() { println("eating grass") }
 
-// Does not compile
-//func Nourish[T Dog | Person | Robot | Slug](subject T) {
-//	subject.Eat()
-//}
+type Eater interface {
+	Eat()
+}
 
-func Speak[T Dog | Person | Robot | Slug](subject T) {
+func Poly[T Dog | Person | Robot | Slug](subject T) {
+	switch subjectTyped := any(subject).(type) {
+	case Eater:
+		subjectTyped.Eat()
+	}
 	switch subjectTyped := any(subject).(type) {
 	case Dog:
 		subjectTyped.Bark()
@@ -35,38 +38,9 @@ func Speak[T Dog | Person | Robot | Slug](subject T) {
 	}
 }
 
-func Poly[T Dog | Person | Robot | Slug](subject T) {
-	switch subjectTyped := any(subject).(type) {
-	case Dog:
-		subjectTyped.Eat()
-	case Person:
-		subjectTyped.Eat()
-	case Robot:
-		subjectTyped.Eat()
-	case Slug:
-		subjectTyped.Eat()
-	}
-	Speak(subject)
-}
-
-type Eater interface {
-	Eat()
-}
-func Poly2[T Dog | Person | Robot | Slug](subject T) {
-	switch subjectTyped := any(subject).(type) {
-	case Eater:
-		subjectTyped.Eat()
-	}
-	Speak(subject)
-}
-
 func main() {
 	Poly(Dog{})
 	Poly(Person{})
 	Poly(Robot{})
 	Poly(Slug{})
-	Poly2(Dog{})
-	Poly2(Person{})
-	Poly2(Robot{})
-	Poly2(Slug{})
 }
