91c053121cf49b581e42edd3a191fd2c6f1b7299
Bruce Eckel <BruceTEckel@gmail.com>
2022-09-15 14:34:52 -0600

    Fix Inheritance.scala

--- src/scala/Inheritance.scala
+++ src/scala/Inheritance.scala
@@ -6,20 +6,20 @@ trait Base:
     def speak(): Unit
 
 class Dog extends Base:
-    def consume() = println("eating dog food")
-    def communicate() = println("woof")
+    def eat() = println("eating dog food")
+    def speak() = println("woof")
 
 class Person extends Base:
-    def consume() = println("eating pizza")
-    def communicate() = println("hello")
+    def eat() = println("eating pizza")
+    def speak() = println("hello")
 //---
 class Robot extends Base:
-    def consume() = println("charging")
-    def communicate() = println("operational")
+    def eat() = println("charging")
+    def speak() = println("operational")
 
 class Slug extends Base:
-    def consume() = println("eating grass")
-    def communicate() = {}
+    def eat() = println("eating grass")
+    def speak() = {}
 
 def nourish(x: Base): Unit =
     x.eat()
